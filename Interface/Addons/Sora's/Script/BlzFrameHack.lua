be088d20acfcff5f789220462ec92622f0b61534
Coolkid <s196774065@gmail.com>
2012-02-16 18:41:57 +0800

    任务移动

--- Interface/Addons/Sora's/Script/BlzFrameHack.lua
+++ Interface/Addons/Sora's/Script/BlzFrameHack.lua
@@ -16,12 +16,46 @@ for i = 1, 4 do
 end
 
 -- 移动任务追踪框体
-local WatchFrame = _G["WatchFrame"]
-WatchFrame:ClearAllPoints()	
-WatchFrame.ClearAllPoints = function() end
-WatchFrame:SetPoint("RIGHT", UIParent, -20, -10)
-WatchFrame.SetPoint = function() end
-WatchFrame:SetHeight(400)  
+local wf = WatchFrame
+local wfmove = false 
+
+wf:SetMovable(true);
+wf:SetClampedToScreen(false); 
+wf:ClearAllPoints()
+wf:SetPoint("TOPRIGHT", UIParent, "TOPRIGHT", -35, -200)
+wf:SetWidth(250)
+wf:SetHeight(500)
+
+wf:SetUserPlaced(true)
+wf.SetPoint = function() end
+wfg = CreateFrame("Frame")
+wfg:SetPoint("TOPLEFT", wf, "TOPLEFT")
+wfg:SetPoint("BOTTOMRIGHT", wf, "BOTTOMRIGHT")
+wfg.text = S.MakeFontString(wfg, 10)
+wfg.text:SetText("点我拖动")
+wfg.text:SetPoint("TOP", wfg, "TOP")
+S.MakeShadow(wfg, 3)
+S.MakeBG(wfg, 0)
+wfg:Hide()
+local function WATCHFRAMELOCK()
+	if wfmove == false then
+		wfmove = true
+		wfg:Show()
+		print("|cffFFD700任务追踪框|r |cff228B22解锁|r")
+		wf:EnableMouse(true);
+		wf:RegisterForDrag("LeftButton"); 
+		wf:SetScript("OnDragStart", wf.StartMoving); 
+		wf:SetScript("OnDragStop", wf.StopMovingOrSizing);
+	elseif wfmove == true then
+		wf:EnableMouse(false);
+		wfmove = false
+		wfg:Hide()
+		print("|cffFFD700任务追踪框|r |cffFF0000锁定|r")
+	end
+end
+
+SLASH_WATCHFRAMELOCK1 = "/wf"
+SlashCmdList["WATCHFRAMELOCK"] = WATCHFRAMELOCK
 
 -- 屏蔽系统红字提示
 local Event = CreateFrame("Frame")
